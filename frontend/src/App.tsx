4aa825f93668964d53791993f52720e400c90a78
Galbaxx <victor.kuenstler@outlook.com>
2018-11-06 10:35:50 +0100

    Add react-digraph-editor example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,121 +1,73 @@
 import React, { Component, Fragment } from 'react';
-import * as PIXI from 'pixi.js';
-import * as d3 from 'd3';
-import * as d3force from 'd3-force';
-import './GraphData.ts';
 import './App.css';
-import testData from './GraphData';
-import { Graph, d3Node, d3Link } from './types';
+import * as dagre from 'dagre';
+import GraphVis from 'react-digraph-editor';
 
-interface IMainState {}
-interface IMainProps {}
-class App extends Component<IMainProps, IMainState> {
-  graphCanvas: HTMLDivElement | null;
+class App extends Component<any, any>{
+  graph: dagre.graphlib.Graph;
 
   constructor(props: any) {
     super(props);
-    this.graphCanvas = null;
+    this.state = {
+      isVisible: true
+    }
+
+    this.graph = new dagre.graphlib.Graph();
+    this.generateGraph();
   }
 
-  componentDidMount() {
-    let width: number = 800;
-    let height: number = 500;
+  generateGraph = () => {
+    this.graph.setGraph({});
 
-    let stage = new PIXI.Container();
-    let render = PIXI.autoDetectRenderer(width, height, {
-      antialias: true,
-      resolution: 2,
-      transparent: true,
-      autoResize: true
+    // Default to assigning a new object as a label for each new edge.
+    this.graph.setDefaultEdgeLabel(function() {
+      return {};
     });
 
-    let color = (function() {
-      let scale: any = d3.scaleOrdinal(d3.schemeCategory10);
-      return (num: number) => parseInt(scale(num).slice(1), 16);
-    })();
+    // Add nodes to the graph. The first argument is the node id. The second is
+    // metadata about the node. In this case we're going to add labels to each of
+    // our nodes.
+    this.graph.setNode('kspacey', { label: 'Kevin Spacey', width: 144, height: 100 });
+    this.graph.setNode('swilliams', { label: 'Saul Williams', width: 160, height: 100 });
+    this.graph.setNode('bpitt', { label: 'Brad Pitt', width: 108, height: 100 });
+    this.graph.setNode('hford', { label: 'Harrison Ford', width: 168, height: 100 });
+    this.graph.setNode('lwilson', { label: 'Luke Wilson', width: 144, height: 100 });
+    this.graph.setNode('kbacon', { label: 'Kevin Bacon', width: 121, height: 100 });
 
-    let simulation = d3
-      .forceSimulation()
-      .force('link', d3.forceLink().id((d: any) => d.id))
-      .force('charge', d3.forceManyBody())
-      .force('center', d3.forceCenter(width / 2, height / 2));
+    // Add edges to the graph.
+    this.graph.setEdge('kspacey', 'swilliams');
+    this.graph.setEdge('swilliams', 'kbacon');
+    this.graph.setEdge('bpitt', 'kbacon');
+    this.graph.setEdge('hford', 'lwilson');
+    this.graph.setEdge('lwilson', 'kbacon');
 
-    let links = new PIXI.Graphics();
+    // layout the graph
+    dagre.layout(this.graph);
+  }
 
-    stage.addChild(links);
-
-    testData.nodes.forEach((node: d3Node) => {
-      node.gfx = new PIXI.Graphics();
-      node.gfx.lineStyle(1.5, 0xffffff);
-      node.gfx.beginFill(color(node.group));
-      node.gfx.drawCircle(0, 0, 10);
-      stage.addChild(node.gfx);
-    });
-
-    simulation.nodes(testData.nodes).on('tick', ticked);
-
-    function ticked() {
-      testData.nodes.forEach(node => {
-        let { x, y, gfx } = node;
-        gfx!.position = new PIXI.Point(x, y);
-      });
-
-      links.clear();
-      links.alpha = 0.6;
-
-      testData.links.forEach((link: d3Link) => {
-        let { source, target } = link;
-        links.lineStyle(Math.sqrt(link.value), 0x999999);
-        //  @ts-ignore
-        links.moveTo(source.x, source.y).lineTo(target.x, target.y);
-      });
-      links.endFill();
-      render.render(stage);
-    }
-
-    simulation
-      .force<d3force.ForceLink<any, d3Link>>('link')!
-      .links(testData.links);
-    render.render(stage);
-    console.log(simulation);
-
-    this.graphCanvas!.appendChild(render.view);
-
-    d3.select(render.view).call(
-      d3
-        .drag()
-        .container(render.view)
-        // @ts-ignore
-        .subject(() => simulation.find(d3.event.x, d3.event.y))
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended)
-    );
-
-    function dragstarted() {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d3.event.subject.fx = d3.event.subject.x;
-      d3.event.subject.fy = d3.event.subject.y;
-    }
-    function dragged() {
-      d3.event.subject.fx = d3.event.x;
-      d3.event.subject.fy = d3.event.y;
-    }
-    function dragended() {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d3.event.subject.fx = null;
-      d3.event.subject.fy = null;
-    }
+  hideGraph = () => {
+    this.setState({ isVisible: !this.state.isVisible });
+    console.log(this.graph);
   }
 
   render() {
-    let component = this;
+    let config = {
+      width: screen.width,
+      height: 500,
+      antialias: true,
+      transparent: false,
+      resolution: 2,
+      autoResize: true,
+      graph: this.graph
+    };
+
+    console.log('test');
+
     return (
-      <div
-        ref={thisDiv => {
-          component.graphCanvas = thisDiv;
-        }}
-      />
+      <div className="App">
+        {this.state.isVisible ? <GraphVis {...config}/> : null}
+        <button onClick={this.hideGraph}>Hide Graph</button>
+      </div>
     );
   }
 }

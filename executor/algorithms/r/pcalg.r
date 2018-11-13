9f04473cd1b1d8abbba6377886762b4bb3b85c44
Tobias Nack <tobiasnack13@googlemail.com>
2018-11-13 16:04:06 +0100

    Add Code TODOs and import pcalg

---
+++ executor/algorithms/r/pcalg.r
@@ -0,0 +1,11 @@
+library(pcalg)
+
+# TODO: Read Command line arguments and hyperparameters
+
+# TODO: Load Relevant data
+
+# TODO: Bring into relavant format
+
+result_graph <- pc()
+
+# TODO: Write Result Graph to endpoint

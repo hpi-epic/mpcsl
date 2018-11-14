db9713c64de921f3102be9e403a1755b596736b5
Alexander Kastius <alex.kastius@qwrz.de>
2018-11-14 14:15:51 +0100

    Fix setup of database

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,4 +5,14 @@ APP_HOST = os.environ.get('APP_HOST', '0.0.0.0')
 APP_PORT = os.environ.get('APP_PORT', '5000')
 
 # Database
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+DB_TYPE = os.environ.get('DB_TYPE', 'postgresql')
+DB_HOST = os.environ.get('DB_HOST', 'database')
+DB_DATABASE = os.environ.get('DB_DATABASE', 'postgres')
+DB_USER = os.environ.get('DB_USER', 'postgres')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'postgres')
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI',
+    DB_TYPE + '://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + '/' + DB_DATABASE
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'false').lower() == 'true'

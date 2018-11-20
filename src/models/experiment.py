09f0235079b65f44cba9ffa723bed009c1466d9d
Tobias Nack <tobiasnack13@googlemail.com>
2018-11-20 14:55:05 +0100

    Add Experiment Model

--- src/models/experiment.py
+++ src/models/experiment.py
@@ -3,7 +3,14 @@ from src.models.base import BaseModel, BaseSchema
 
 
 class Experiment(BaseModel):
-    pass
+    alpha = db.Column(db.Float)
+
+    independence_test = db.Column(db.String)
+
+    dataset_id = db.Column(db.Integer)
+    # TODO: Dataset relationship
+
+    cores = db.Column(db.Integer)
 
 
 class ExperimentSchema(BaseSchema):

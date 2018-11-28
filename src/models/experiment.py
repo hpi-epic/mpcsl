0f4a6e11bc5c3e6cbd9be4cb8851c0ce36b04bfb
Tobias Nack <tobiasnack13@googlemail.com>
2018-11-28 16:12:43 +0100

    Add choice of independence test to pcalg implementation (#14)

--- src/models/experiment.py
+++ src/models/experiment.py
@@ -1,11 +1,19 @@
+import enum
+
 from src.db import db
 from src.models.base import BaseModel, BaseSchema
+
+
+class IndependenceTest(str, enum.Enum):
+    gaussCI = "gaussCI"
+    binCI = "binCI"
+    disCI = "disCI"
 
 
 class Experiment(BaseModel):
     alpha = db.Column(db.Float)
 
-    independence_test = db.Column(db.String)
+    independence_test = db.Column(db.Enum(IndependenceTest))
 
     dataset_id = db.Column(db.Integer, db.ForeignKey('dataset.id'))
     dataset = db.relationship('Dataset')

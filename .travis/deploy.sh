5c04dd4f788071c006f47185f0a2fbfef20493cb
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2018-12-05 23:24:15 +0100

    Fail early
    set -e doesn't work in sub shell

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-#if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]] ; then
-if [[ $TRAVIS_BRANCH = "master" ]]; then
-    set -e
-    echo "Deploying the master"
-    eval "$(ssh-agent -s)" # start the ssh agent
-    chmod 600 .travis/deploy_key
-    echo -e "Host deploy@mpci.epic-hpi.de\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-    ssh-add .travis/deploy_key
-    git remote add deploy ssh://deploy@mpci.epic-hpi.de:/home/deploy/mpci
-    git push deploy master
-    ssh deploy@mpci.epic-hpi.de /bin/bash <<EOF
+#if [[ $TRAVIS_BRANCH = "master"  ]] ; then
+if [[ $TRAVIS_BRANCH != "master" || $TRAVIS_PULL_REQUEST != "false" ]]; then
+    exit 0
+fi
+
+set -e
+echo "Deploying the master"
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy_key
+echo -e "Host deploy@mpci.epic-hpi.de\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add .travis/deploy_key
+git remote add deploy ssh://deploy@mpci.epic-hpi.de:/home/deploy/mpci
+git push deploy master
+ssh deploy@mpci.epic-hpi.de /bin/bash <<EOF
       cd mpci
       docker-compose down
       docker-compose build
       docker-compose up --detach
 EOF
-fi

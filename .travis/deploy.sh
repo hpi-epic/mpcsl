fe0b5c6ff3fffdfb4643b585be71198665fc1c26
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2018-12-05 22:38:23 +0100

    Fail deployment early

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
-#if [[ "$TRAVIS_BRANCH" = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
-if [[ "$TRAVIS_BRANCH" = "master" ]]; then
+#if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]] ; then
+if [[ $TRAVIS_BRANCH = "master" ]]; then
     echo "Deploying the master"
     eval "$(ssh-agent -s)" # start the ssh agent
     chmod 600 .travis/deploy_key
@@ -9,7 +10,7 @@ if [[ "$TRAVIS_BRANCH" = "master" ]]; then
     ssh-add .travis/deploy_key
     git remote add deploy ssh://deploy@mpci.epic-hpi.de:/home/deploy/mpci
     git push deploy master
-    ssh deploy@mpci.epic-hpi.de <<EOF
+    ssh deploy@mpci.epic-hpi.de /bin/bash <<EOF
       cd mpci
       docker-compose down
       docker-compose build

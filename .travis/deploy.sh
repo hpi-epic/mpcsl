af132d883899c839d8ea885db814e304d3db1b20
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2018-12-05 21:56:52 +0100

    Fix paths

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,9 +4,9 @@
 if [[ "$TRAVIS_BRANCH" = "master" ]]; then
     echo "Deploying the master"
     eval "$(ssh-agent -s)" # start the ssh agent
-    chmod 600 ./deploy_key
+    chmod 600 .travis/deploy_key
     echo -e "Host deploy@mpci.epic-hpi.de\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-    ssh-add ./deploy_key
+    ssh-add .travis/deploy_key
     git remote add deploy ssh://deploy@mpci.epic-hpi.de:/home/deploy/mpci
     git push deploy master
     ssh deploy@mpci.epic-hpi.de <<EOF

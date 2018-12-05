52eee822f6ba49e855e3f404ca7878c98a0d90c5
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2018-12-05 21:52:38 +0100

    Test deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-
-if [[ "$TRAVIS_BRANCH" = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
+#if [[ "$TRAVIS_BRANCH" = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
+if [[ "$TRAVIS_BRANCH" = "master" ]]; then
     echo "Deploying the master"
     eval "$(ssh-agent -s)" # start the ssh agent
     chmod 600 ./deploy_key
-    echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    echo -e "Host deploy@mpci.epic-hpi.de\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
     ssh-add ./deploy_key
     git remote add deploy ssh://deploy@mpci.epic-hpi.de:/home/deploy/mpci
     git push deploy master

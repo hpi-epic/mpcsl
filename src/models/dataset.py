127b176cacc83cc8e85c4869521e38e1159f10b6
Daniel Theveßen <danthe96@gmx.de>
2018-12-11 14:19:06 +0100

    Add description and creation time fields to dataset

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql import func
+
 from src.db import db
 from src.models.base import BaseModel, BaseSchema
 
@@ -5,6 +7,8 @@ from src.models.base import BaseModel, BaseSchema
 class Dataset(BaseModel):
     load_query = db.Column(db.String)
     name = db.Column(db.String)
+    description = db.Column(db.String)
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
 
 class DatasetSchema(BaseSchema):

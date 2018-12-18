76f0849fde1ad15cf7196f6aaa2289bbdac385c1
Alexander Kastius <alex.kastius@qwrz.de>
2018-12-18 17:19:38 +0100

    nginx setup (#34)
    * Add nginx setup
    
    * Add conf files and setup for ui build
    
    * Add submodule dir
    
    * Fix travis yml
    
    * Update production setup
    
    * Fix wrong urls in r script
    
    * Optimize Docker image

--- src/master/routes.py
+++ src/master/routes.py
@@ -5,15 +5,19 @@ from .resources import DatasetLoadResource, DatasetListResource, \
 from src.master.executor.executor import ExecutorResource
 
 
+def base_url(url):
+    return '/api' + url
+
+
 def set_up_routes(api):
-    api.add_resource(DatasetLoadResource, '/dataset/<int:dataset_id>/load')
-    api.add_resource(DatasetResource, '/dataset/<int:dataset_id>')
-    api.add_resource(DatasetListResource, '/datasets')
-    api.add_resource(ExperimentResource, '/experiment/<int:experiment_id>')
-    api.add_resource(ExperimentJobListResource, '/experiment/<int:experiment_id>/jobs')
-    api.add_resource(ExecutorResource, '/experiment/<int:experiment_id>/start')
-    api.add_resource(ExperimentListResource, '/experiments')
-    api.add_resource(JobResource, '/job/<int:job_id>')
-    api.add_resource(JobResultResource, '/job/<int:job_id>/result')
-    api.add_resource(JobListResource, '/jobs')
-    api.add_resource(ResultListResource, '/results')
+    api.add_resource(DatasetLoadResource, base_url('/dataset/<int:dataset_id>/load'))
+    api.add_resource(DatasetResource, base_url('/dataset/<int:dataset_id>'))
+    api.add_resource(DatasetListResource, base_url('/datasets'))
+    api.add_resource(ExperimentResource, base_url('/experiment/<int:experiment_id>'))
+    api.add_resource(ExperimentJobListResource, base_url('/experiment/<int:experiment_id>/jobs'))
+    api.add_resource(ExecutorResource, base_url('/experiment/<int:experiment_id>/start'))
+    api.add_resource(ExperimentListResource, base_url('/experiments'))
+    api.add_resource(JobResource, base_url('/job/<int:job_id>'))
+    api.add_resource(JobResultResource, base_url('/job/<int:job_id>/result'))
+    api.add_resource(JobListResource, base_url('/jobs'))
+    api.add_resource(ResultListResource, base_url('/results'))

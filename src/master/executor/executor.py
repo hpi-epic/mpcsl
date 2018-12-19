0607351d1601b7c0285c2e714bbeefd7a93df614
Alexander Kastius <alex.kastius@qwrz.de>
2018-12-19 15:58:36 +0100

    Add API_HOST and passing into r script

--- src/master/executor/executor.py
+++ src/master/executor/executor.py
@@ -5,6 +5,7 @@ from flask_restful_swagger_2 import swagger
 from flask import current_app
 from flask_restful import Resource
 
+from src.master.config import API_HOST
 from src.master.helpers.io import marshal
 from src.db import db
 from src.master.helpers.swagger import get_default_response
@@ -44,7 +45,8 @@ class ExecutorResource(Resource):
         r_process = Popen([
             'Rscript', 'src/master/executor/algorithms/r/pcalg.r',
             '-j', str(new_job.id),
-            '-d', str(experiment.dataset_id)
+            '-d', str(experiment.dataset_id),
+            '-ah', str(API_HOST)
         ] + params)
 
         new_job.pid = r_process.pid

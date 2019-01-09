547a380e2d8c72cda3266c72e79325532ecdd3f9
Daniel Thevessen <danthe96@gmx.de>
2019-01-09 15:28:52 +0100

    Test integration tests last

--- test/integration/test_executor.py
+++ test/integration/test_executor.py
@@ -2,6 +2,7 @@ import time
 
 import numpy as np
 import pandas as pd
+import pytest
 import requests
 
 from src.db import db
@@ -13,6 +14,7 @@ from .base import BaseIntegrationTest
 
 class GaussExecutorTest(BaseIntegrationTest):
 
+    @pytest.mark.run(order=-4)
     def test_r_execution(self):
         # Set up fixtures
         ds = DatasetFactory(
@@ -62,102 +64,105 @@ class GaussExecutorTest(BaseIntegrationTest):
 
 class DiscreteExecutorTest(BaseIntegrationTest):
 
-        def test_r_execution_discrete(self):
-            # Set up fixtures
-            ds = DatasetFactory(
-                load_query="SELECT * FROM test_data"
-            )
-            db.session.execute("""
-                CREATE TABLE IF NOT EXISTS test_data (
-                    a int,
-                    b int,
-                    c int
-                );
-            """)
-            source = np.random.randint(0, 10, (50, 3))
-            for l in source:
-                db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
-            ex = ExperimentFactory(dataset=ds)
-            ex.parameters['independence_test'] = 'disCI'
-            db.session.commit()
+    @pytest.mark.run(order=-3)
+    def test_r_execution_discrete(self):
+        # Set up fixtures
+        ds = DatasetFactory(
+            load_query="SELECT * FROM test_data"
+        )
+        db.session.execute("""
+            CREATE TABLE IF NOT EXISTS test_data (
+                a int,
+                b int,
+                c int
+            );
+        """)
+        source = np.random.randint(0, 10, (50, 3))
+        for l in source:
+            db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
+        ex = ExperimentFactory(dataset=ds)
+        ex.parameters['independence_test'] = 'disCI'
+        db.session.commit()
 
-            # When
-            job_r = requests.post(self.url_for(ExecutorResource, experiment_id=ex.id))
-            assert job_r.status_code == 200
+        # When
+        job_r = requests.post(self.url_for(ExecutorResource, experiment_id=ex.id))
+        assert job_r.status_code == 200
 
-            job = db.session.query(Job).get(job_r.json()['id'])
+        job = db.session.query(Job).get(job_r.json()['id'])
 
-            result = None
-            i = 0
-            while result is None:
-                if i > 15:
-                    raise TimeoutError
-                time.sleep(1)
+        result = None
+        i = 0
+        while result is None:
+            if i > 15:
+                raise TimeoutError
+            time.sleep(1)
 
-                # If this fails because of transaction abort, check R script (use same session)
-                result = db.session.query(Result).filter(Result.job == job).first()
-                i += 1
+            # If this fails because of transaction abort, check R script (use same session)
+            result = db.session.query(Result).filter(Result.job == job).first()
+            i += 1
 
-            assert result.job_id == job.id
-            assert result.job.experiment_id == ex.id
-            assert result.start_time == job.start_time
+        assert result.job_id == job.id
+        assert result.job.experiment_id == ex.id
+        assert result.start_time == job.start_time
 
-            nodes = db.session.query(Node).all()
-            for node in nodes:
-                assert node.name in ['a', 'b', 'c']
-            assert len(nodes) == 3
+        nodes = db.session.query(Node).all()
+        for node in nodes:
+            assert node.name in ['a', 'b', 'c']
+        assert len(nodes) == 3
 
 
 class BinaryExecutorTest(BaseIntegrationTest):
 
-        def test_r_execution_binary(self):
-            # Set up fixtures
-            ds = DatasetFactory(
-                load_query="SELECT * FROM test_data"
-            )
-            db.session.execute("""
-                CREATE TABLE IF NOT EXISTS test_data (
-                    a int,
-                    b int,
-                    c int
-                );
-            """)
-            source = np.random.randint(0, 2, (50, 3))
-            for l in source:
-                db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
-            ex = ExperimentFactory(dataset=ds)
-            ex.parameters['independence_test'] = 'binCI'
-            db.session.commit()
+    @pytest.mark.run(order=-2)
+    def test_r_execution_binary(self):
+        # Set up fixtures
+        ds = DatasetFactory(
+            load_query="SELECT * FROM test_data"
+        )
+        db.session.execute("""
+            CREATE TABLE IF NOT EXISTS test_data (
+                a int,
+                b int,
+                c int
+            );
+        """)
+        source = np.random.randint(0, 2, (50, 3))
+        for l in source:
+            db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
+        ex = ExperimentFactory(dataset=ds)
+        ex.parameters['independence_test'] = 'binCI'
+        db.session.commit()
 
-            # When
-            job_r = requests.post(self.url_for(ExecutorResource, experiment_id=ex.id))
-            assert job_r.status_code == 200
+        # When
+        job_r = requests.post(self.url_for(ExecutorResource, experiment_id=ex.id))
+        assert job_r.status_code == 200
 
-            job = db.session.query(Job).get(job_r.json()['id'])
+        job = db.session.query(Job).get(job_r.json()['id'])
 
-            result = None
-            i = 0
-            while result is None:
-                if i > 15:
-                    raise TimeoutError
-                time.sleep(1)
+        result = None
+        i = 0
+        while result is None:
+            if i > 15:
+                raise TimeoutError
+            time.sleep(1)
 
-                # If this fails because of transaction abort, check R script (use same session)
-                result = db.session.query(Result).filter(Result.job == job).first()
-                i += 1
+            # If this fails because of transaction abort, check R script (use same session)
+            result = db.session.query(Result).filter(Result.job == job).first()
+            i += 1
 
-            assert result.job_id == job.id
-            assert result.job.experiment_id == ex.id
-            assert result.start_time == job.start_time
+        assert result.job_id == job.id
+        assert result.job.experiment_id == ex.id
+        assert result.start_time == job.start_time
 
-            nodes = db.session.query(Node).all()
-            for node in nodes:
-                assert node.name in ['a', 'b', 'c']
-            assert len(nodes) == 3
+        nodes = db.session.query(Node).all()
+        for node in nodes:
+            assert node.name in ['a', 'b', 'c']
+        assert len(nodes) == 3
 
 
 class SepsetExecutorTest(BaseIntegrationTest):
 
+    @pytest.mark.run(order=-1)
     def test_r_execution_with_sepsets(self):
         ds = DatasetFactory(
             load_query="SELECT * FROM test_data"

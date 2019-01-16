b5a5a4e8012d81537e800b3f6234da43fad9b732
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2019-01-16 19:47:14 +0100

    Fix test

--- test/unit/master/resources/test_job.py
+++ test/unit/master/resources/test_job.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import signal
+import factory
 from unittest.mock import patch
 
 
@@ -60,12 +61,15 @@ class JobTest(BaseResourceTest):
     def test_delete_job(self):
         # Given
         job = JobFactory()
+        job_pgid = factory.Faker('pyint')
 
         # When
-        with patch('src.master.resources.jobs.os.kill') as m:
-            result = self.delete(self.url_for(JobResource, job_id=job.id))
+        with patch('src.master.resources.jobs.os.getpgid', return_value=job_pgid) as mock_pgid:
+            with patch('src.master.resources.jobs.os.killpg') as mock_killpg:
+                result = self.delete(self.url_for(JobResource, job_id=job.id))
 
-            m.assert_called_once_with(job.pid, signal.SIGTERM)
+                mock_pgid.assert_called_once_with(job.pid)
+                mock_killpg.assert_called_once_with(job_pgid, signal.SIGTERM)
 
         # Then
         assert result['id'] == job.id

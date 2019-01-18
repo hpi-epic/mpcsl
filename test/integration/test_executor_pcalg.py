33aacd27b32029aeea61c8fb4d30b49e555ab6ac
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2019-01-19 00:49:23 +0100

    Refactor integration tests to common code base

--- test/integration/test_executor_pcalg.py
+++ test/integration/test_executor_pcalg.py
@@ -1,96 +1,9 @@
-import time
-
-import numpy as np
-import pandas as pd
 import pytest
-import requests
 
 from src.db import db
-from src.models import Result, Job, Node, Edge, Sepset
-from src.master.executor import ExecutorResource
-from test.factories import ExperimentFactory, DatasetFactory
+from src.models import Node, Edge, Sepset
+from test.factories import ExperimentFactory
 from .base import BaseIntegrationTest
-
-
-def setup_dataset_gauss():
-        ds = DatasetFactory(
-            load_query="SELECT * FROM test_data"
-        )
-        db.session.execute("""
-            CREATE TABLE IF NOT EXISTS test_data (
-                a float,
-                b float,
-                c float
-            );
-        """)
-        mean = [0, 5, 10]
-        cov = [[1, 0, 0], [0, 10, 0], [0, 0, 20]]
-        source = np.random.multivariate_normal(mean, cov, size=50)
-        for l in source:
-            db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
-        return ds
-
-
-def setup_dataset_discrete():
-    ds = DatasetFactory(
-        load_query="SELECT * FROM test_data"
-    )
-    db.session.execute("""
-        CREATE TABLE IF NOT EXISTS test_data (
-            a int,
-            b int,
-            c int
-        );
-    """)
-    source = np.random.randint(0, 10, (50, 3))
-    for l in source:
-        db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
-    return ds
-
-
-def setup_dataset_binary():
-    ds = DatasetFactory(
-        load_query="SELECT * FROM test_data"
-    )
-    db.session.execute("""
-        CREATE TABLE IF NOT EXISTS test_data (
-            a int,
-            b int,
-            c int
-        );
-    """)
-    source = np.random.randint(0, 2, (50, 3))
-    for l in source:
-        db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
-    return ds
-
-
-def setup_dataset_cooling_house():
-    ds = DatasetFactory(
-        load_query="SELECT * FROM test_data"
-    )
-    df = pd.read_csv('test/fixtures/coolinghouse_1k.csv', index_col=0)
-    df.to_sql('test_data', con=db.engine, index=False)
-    return ds
-
-
-def run_experiment(self, experiment):
-    job_r = requests.post(self.url_for(ExecutorResource, experiment_id=experiment.id))
-    assert job_r.status_code == 200
-
-    job = db.session.query(Job).get(job_r.json()['id'])
-
-    result = None
-    i = 0
-    while result is None:
-        if i > 15:
-            raise TimeoutError
-        time.sleep(1)
-
-        # If this fails because of transaction abort, check R script (use same session)
-        result = db.session.query(Result).filter(Result.job == job).first()
-        i += 1
-    return job, result
 
 
 class GaussExecutorTest(BaseIntegrationTest):
@@ -98,11 +11,11 @@ class GaussExecutorTest(BaseIntegrationTest):
     @pytest.mark.run(order=-4)
     def test_r_execution_gauss(self):
         # Given
-        ex = ExperimentFactory(dataset=setup_dataset_gauss())
+        ex = ExperimentFactory(dataset=self.setup_dataset_gauss())
         db.session.commit()
 
         # When
-        job, result = run_experiment(self, ex)
+        job, result = self.run_experiment(ex)
 
         # Then
         assert result.job_id == job.id
@@ -120,12 +33,12 @@ class DiscreteExecutorTest(BaseIntegrationTest):
     @pytest.mark.run(order=-3)
     def test_r_execution_discrete(self):
         # Given
-        ex = ExperimentFactory(dataset=setup_dataset_discrete())
+        ex = ExperimentFactory(dataset=self.setup_dataset_discrete())
         ex.parameters['independence_test'] = 'disCI'
         db.session.commit()
 
         # When
-        job, result = run_experiment(self, ex)
+        job, result = self.run_experiment(ex)
 
         # Then
         assert result.job_id == job.id
@@ -143,12 +56,12 @@ class BinaryExecutorTest(BaseIntegrationTest):
     @pytest.mark.run(order=-2)
     def test_r_execution_binary(self):
         # Given
-        ex = ExperimentFactory(dataset=setup_dataset_binary())
+        ex = ExperimentFactory(dataset=self.setup_dataset_binary())
         ex.parameters['independence_test'] = 'binCI'
         db.session.commit()
 
         # When
-        job, result = run_experiment(self, ex)
+        job, result = self.run_experiment(ex)
 
         # Then
         assert result.job_id == job.id
@@ -166,12 +79,12 @@ class SepsetExecutorTest(BaseIntegrationTest):
     @pytest.mark.run(order=-1)
     def test_r_execution_with_sepsets(self):
         # Given
-        ex = ExperimentFactory(dataset=setup_dataset_cooling_house())
+        ex = ExperimentFactory(dataset=self.setup_dataset_cooling_house())
         ex.parameters['alpha'] = 0.05
         db.session.commit()
 
         # When
-        job, result = run_experiment(self, ex)
+        job, result = self.run_experiment(ex)
 
         # Then
         assert result.job_id == job.id

089c8c349af7e52071d14146c5875a683fde94e5
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2019-01-19 01:25:42 +0100

    Add parallelPC algorithm (#56)
    * Add parallel PC, update algorithms
    
    * Refactor integration tests to common code base

--- test/integration/base.py
+++ test/integration/base.py
@@ -1,4 +1,7 @@
 import os
+import requests
+import pandas as pd
+import numpy as np
 import signal
 import time
 from unittest import TestCase
@@ -8,6 +11,9 @@ from urllib.request import urlopen
 
 from src.master.appfactory import AppFactory
 from src.db import db
+from src.master.executor import ExecutorResource
+from src.models import Result, Job
+from test.factories import DatasetFactory
 
 
 class BaseIntegrationTest(TestCase):
@@ -74,3 +80,83 @@ class BaseIntegrationTest(TestCase):
     def url_for(self, resource, **values):
         adapter = self.app.url_map.bind('localhost:5000')
         return adapter.build(resource.endpoint, values, force_external=True)
+
+    @staticmethod
+    def setup_dataset_gauss():
+        ds = DatasetFactory(
+            load_query="SELECT * FROM test_data"
+        )
+        db.session.execute("""
+                CREATE TABLE IF NOT EXISTS test_data (
+                    a float,
+                    b float,
+                    c float
+                );
+            """)
+        mean = [0, 5, 10]
+        cov = [[1, 0, 0], [0, 10, 0], [0, 0, 20]]
+        source = np.random.multivariate_normal(mean, cov, size=50)
+        for l in source:
+            db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
+        return ds
+
+    @staticmethod
+    def setup_dataset_discrete():
+        ds = DatasetFactory(
+            load_query="SELECT * FROM test_data"
+        )
+        db.session.execute("""
+            CREATE TABLE IF NOT EXISTS test_data (
+                a int,
+                b int,
+                c int
+            );
+        """)
+        source = np.random.randint(0, 10, (50, 3))
+        for l in source:
+            db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
+        return ds
+
+    @staticmethod
+    def setup_dataset_binary():
+        ds = DatasetFactory(
+            load_query="SELECT * FROM test_data"
+        )
+        db.session.execute("""
+            CREATE TABLE IF NOT EXISTS test_data (
+                a int,
+                b int,
+                c int
+            );
+        """)
+        source = np.random.randint(0, 2, (50, 3))
+        for l in source:
+            db.session.execute("INSERT INTO test_data VALUES ({0})".format(",".join([str(e) for e in l])))
+        return ds
+
+    @staticmethod
+    def setup_dataset_cooling_house():
+        ds = DatasetFactory(
+            load_query="SELECT * FROM test_data"
+        )
+        df = pd.read_csv('test/fixtures/coolinghouse_1k.csv', index_col=0)
+        df.to_sql('test_data', con=db.engine, index=False)
+        return ds
+
+    def run_experiment(self, experiment):
+        job_r = requests.post(self.url_for(ExecutorResource, experiment_id=experiment.id))
+        assert job_r.status_code == 200
+
+        job = db.session.query(Job).get(job_r.json()['id'])
+
+        result = None
+        i = 0
+        while result is None:
+            if i > 15:
+                raise TimeoutError
+            time.sleep(1)
+
+            # If this fails because of transaction abort, check R script (use same session)
+            result = db.session.query(Result).filter(Result.job == job).first()
+            i += 1
+        return job, result

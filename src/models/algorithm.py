a9b7e0e21cce32a6c67065d3d37f3c9163270513
Daniel Thevessen <danthe96@gmx.de>
2019-02-14 19:31:22 +0100

    Validate algorithm parameters

--- src/models/algorithm.py
+++ src/models/algorithm.py
@@ -1,6 +1,6 @@
 import enum
 
-from marshmallow import fields
+from marshmallow import fields, validates, ValidationError
 from sqlalchemy.ext.mutable import MutableDict
 
 from src.db import db
@@ -23,6 +23,28 @@ class Algorithm(BaseModel):
 
 class AlgorithmSchema(BaseSchema):
     valid_parameters = fields.Dict()
+
+    @validates('valid_parameters')
+    def validate_params(params):
+        # Just for validation
+        for key, val in params.items():
+            if 'type' not in val or val['type'] not in ['str', 'enum', 'int', 'float', 'bool']:
+                raise ValidationError(f'Invalid type for key {key}')
+            if not isinstance(val.get('required', False), bool):
+                raise ValidationError(f'Field `required` must be bool for key {key}')
+
+            if val['type'] == 'enum':
+                if not isinstance(val['values'], list):
+                    raise ValidationError(f'Field `values` must be list for key {key}')
+                for item in val['values']:
+                    if not isinstance(item, str):
+                        raise ValidationError(f'Field `values` must be list of strings for key {key}')
+            elif val['type'] in ['int', 'float']:
+                param_cls = eval(val['type'])
+                if not isinstance(val.get('minimum', param_cls()), param_cls):
+                    raise ValidationError(f'Field `minimum` must be {param_cls} for key {key}')
+                if not isinstance(val.get('maximum', param_cls()), param_cls):
+                    raise ValidationError(f'Field `maximum` must be {param_cls} for key {key}')
 
     class Meta(BaseSchema.Meta):
         model = Algorithm

8b38691d81b9ceea31887e95a7aa994080f2b686
Daniel Thevessen <danthe96@gmx.de>
2019-02-20 16:10:49 +0100

    Merge migrations

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,6 +6,7 @@ from .job import Job, JobSchema, JobStatus
 from .result import Result, ResultSchema
 from .node import Node, NodeSchema
 from .edge import Edge, EdgeSchema
+from .edge_information import EdgeAnnotation, EdgeInformation, EdgeInformationSchema
 from .sepset import Sepset, SepsetSchema
 
 __all__ = [
@@ -27,5 +28,8 @@ __all__ = [
     'SepsetSchema',
     'Job',
     'JobSchema',
-    'JobStatus'
+    'JobStatus',
+    'EdgeAnnotation',
+    'EdgeInformation',
+    'EdgeInformationSchema'
 ]

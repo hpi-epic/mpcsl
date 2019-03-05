406614936429234037d4f49abeda87d3449a037d
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2019-03-05 11:12:00 +0100

    Improve error handling and test dataset change detection (#86)

--- src/master/helpers/database.py
+++ src/master/helpers/database.py
@@ -1,4 +1,6 @@
 from hashlib import blake2b
+from sqlalchemy.exc import DatabaseError
+from werkzeug.exceptions import BadRequest
 
 from src.db import db
 from src.master.db import data_source_connections
@@ -9,11 +11,14 @@ def check_dataset_hash(dataset):
     if dataset.remote_db is not None:
         session = data_source_connections.get(dataset.remote_db, None)
         if session is None:
-            return None
+            raise BadRequest(f'Could not reach database "{dataset.remote_db}"')
     else:
         session = db.session
 
-    result = session.execute(dataset.load_query).fetchone()
+    try:
+        result = session.execute(dataset.load_query).fetchone()
+    except DatabaseError:
+        raise BadRequest(f'Could not execute query "{dataset.load_query}" on database "{dataset.remote_db}"')
 
     hash = blake2b()
     hash.update(str(result).encode())
@@ -25,13 +30,14 @@ def add_dataset_nodes(dataset):
     if dataset.remote_db is not None:
         session = data_source_connections.get(dataset.remote_db, None)
         if session is None:
-            return None
+            raise BadRequest(f'Could not reach database "{dataset.remote_db}"')
     else:
         session = db.session
 
-    result = session.execute(dataset.load_query)
-
-    result = result.fetchone()
+    try:
+        result = session.execute(dataset.load_query).fetchone()
+    except DatabaseError:
+        raise BadRequest(f'Could not execute query "{dataset.load_query}" on database "{dataset.remote_db}"')
 
     for key in result.keys():
         node = Node(name=key, dataset=dataset)

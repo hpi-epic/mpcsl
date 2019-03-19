a6299a33d4e89230c802ccec6256ae93bb23e550
danthe <me@danolithe.com>
2019-03-19 13:16:21 +0000

    Endpoint for external factors (#99)
    * Calculate external factors

--- src/master/resources/nodes.py
+++ src/master/resources/nodes.py
@@ -2,6 +2,8 @@ from flask_restful_swagger_2 import swagger
 from flask_restful import Resource
 from marshmallow import fields
 
+from src.master.helpers.database import load_networkx_graph
+from src.master.helpers.graph import get_potential_confounders
 from src.master.helpers.io import marshal
 from src.master.helpers.swagger import get_default_response
 from src.models import Node, NodeSchema, BaseSchema, Result
@@ -89,4 +91,45 @@ class NodeContextResource(Resource):
             'main_node': main_node,
             'context_nodes': list(context_nodes),
             'edges': edges
+        })
+
+
+class NodeConfounderSchema(BaseSchema, SwaggerMixin):
+    confounders = fields.List(fields.List(fields.Int))
+
+
+class NodeConfounderResource(Resource):
+
+    @swagger.doc({
+        'description': 'Returns confounders to condition for an intervention. '
+                       'If any neighbors are bidirectional, this will return multiple lists of node IDs '
+                       'as the equivalence class contains many possible DAGs.',
+        'parameters': [
+            {
+                'name': 'node_id',
+                'description': 'Node identifier',
+                'in': 'path',
+                'type': 'integer',
+                'required': True
+            },
+            {
+                'name': 'result_id',
+                'description': 'Result identifier',
+                'in': 'path',
+                'type': 'integer',
+                'required': True
+            }
+        ],
+        'responses': get_default_response(NodeContextSchema.get_swagger()),
+        'tags': ['Node']
+    })
+    def get(self, node_id, result_id):
+        node = Node.query.get_or_404(node_id)
+        result = Result.query.get_or_404(result_id)
+
+        graph = load_networkx_graph(result)
+        confounder_list = get_potential_confounders(graph, node.id)
+
+        return marshal(NodeConfounderSchema, {
+            'confounders': confounder_list
         })

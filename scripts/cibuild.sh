9d1c3626ca4088211954935248226475231cea83
Daniel Thevessen <danthe96@gmx.de>
2019-03-20 14:00:52 +0100

    Use new docker-compose version

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,8 +3,6 @@
 # scripts/cibuild.sh: Setup environment for CI to run tests. This is primarily
 #                     designed to run on the continuous integration server.
 
-docker-compose --version
-
 echo "==> Setup project…"
 bash scripts/setup.sh
 

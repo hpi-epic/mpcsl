eed3843f202e07c7a7e909b713908e0317512621
Jonathan Schneider <jonathan.schneider@student.hpi.de>
2019-03-20 12:09:58 +0100

    Change cibuild

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,8 +3,8 @@
 # scripts/cibuild.sh: Setup environment for CI to run tests. This is primarily
 #                     designed to run on the continuous integration server.
 
-echo "==> Update…"
-bash scripts/update.sh
+echo "==> Setup project…"
+bash scripts/setup.sh
 
 echo "==> Running tests…"
 bash scripts/test.sh

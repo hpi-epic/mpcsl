dcb6f30ae22330c4ad090df1ec5e067ec8365f0d
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2019-08-14 11:17:41 +0200

    adjusted bnlearn parameters to include parameter setting the limit of unique values in a variable to consider the variable as discrete one

--- src/executionenvironments/r/bnlearn.r
+++ src/executionenvironments/r/bnlearn.r
@@ -1,6 +1,7 @@
-library(bnlearn)
-library(parallel)
-library(dplyr)
+library(optparse, quietly = T)
+library(bnlearn, quietly = T)
+library(parallel, quietly = T)
+library(dplyr, quietly = T)
 source("/scripts/mpci_utils.r")
 
 
@@ -15,31 +16,23 @@ option_list_v <- list(
                                 help="Independence test used for the bnlearn pc.stable", metavar=""),
                     make_option(c("-a", "--alpha"), type="double", default=0.05,
                                 help="This is a hyperparameter", metavar=""),
-#TODO                    make_option(c("-c", "--cores"), type="integer", default=1,
-#TODO                                help="The number of cores to run the pc-algorithm on", metavar=""),
                     make_option(c("-s", "--subset_size"), type="integer", default=-1,
                                 help="The maximal size of the conditioning sets that are considered", metavar=""),
                     make_option(c("--send_sepsets"), type="integer", default=0,
                                 help="If 1, sepsets will be sent with the results", metavar=""),
                     make_option(c("-v", "--verbose"), type="integer", default=0,
-                                help="More detailed output is provided (with impact on performance)", metavar="")
-                    #make_option(c("--fixed_gaps"), type="character", default=NULL,
-                    #            help="The connections that are removed via prior knowledge", metavar=""),
-                    #make_option(c("--fixed_edges"), type="character", default=NULL,
-                    #            help="The connections that are fixed via prior knowledge", metavar=""),
+                                help="More detailed output is provided (with impact on performance)", metavar=""),
+                    make_option(c("--discrete_limit"), type="integer", default=4,
+                                help="Maximum unique values per variable considered as discrete", metavar="")
 
 );
-
-indepTestDict <- list(gaussCI=gaussCItest, binCI=binCItest, disCI=disCItest)
 
 option_parser <- OptionParser(option_list=option_list_v)
 opt <- parse_args(option_parser)
 
 df <- get_dataset(opt$api_host, opt$dataset_id, opt$job_id)
-#df <- read.csv(url('http://192.168.30.184:5000/api/dataset/17/load'), header=TRUE, check.names=FALSE, row.names = NULL, colClasses = "character")
-
 if (opt$independence_test == "mi-cg") {
-	matrix_df <- df%>%dplyr::mutate_all(funs(if(length(unique(.))<4) as.factor(.)  else as.numeric(as.numeric(.))))
+	matrix_df <- df%>%dplyr::mutate_all(funs(if(length(unique(.))<opt$discrete_limit) as.factor(.)  else as.numeric(as.numeric(.))))
 } else {
 	stop("No valid independence test specified")
 }

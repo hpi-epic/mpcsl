14eb6de75118715ae2d0b7d46e7e267777dc8fe1
MariusDanner <marius@danner.online>
2019-11-04 10:49:21 +0100

    Use db helper for graph creation

--- src/models/result.py
+++ src/models/result.py
@@ -5,7 +5,6 @@ from src.db import db
 from src.models.base import BaseModel, BaseSchema
 from src.models.node import Node
 
-
 class Result(BaseModel):
     job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
     job = db.relationship('Job', backref=db.backref('results'))
@@ -18,15 +17,9 @@ class Result(BaseModel):
 
     @property
     def ground_truth_statistics(self):
-        g1 = nx.DiGraph(id=self.id, name=f'Graph_{self.id}_gt')
-        for node in self.job.experiment.dataset.nodes:
-            g1.add_node(node.id, label=node.name)
-        for node in self.job.experiment.dataset.nodes:
-            for edge in node.edge_froms:
-                if edge.is_ground_truth:
-                    g1.add_edge(edge.from_node.id, edge.to_node.id, id=edge.id, label='', weight=edge.weight)
-
-
+        from src.master.helpers.database import load_networkx_graph
+        g1 = load_networkx_graph(self)
+        
         ground_truth = nx.DiGraph(id=-1, name=f'Graph_{self.id}_gt')
         for node in self.job.experiment.dataset.nodes:
             ground_truth.add_node(node.id, label=node.name)
@@ -34,7 +27,11 @@ class Result(BaseModel):
             for edge in node.edge_froms:
                 if edge.is_ground_truth:
                     ground_truth.add_edge(edge.from_node.id, edge.to_node.id, id=edge.id, label='', weight=1)
-        return {'graph_edit_distance': nx.graph_edit_distance(ground_truth,g1)}
+        
+        ground_truth_statistics = {}
+        ground_truth_statistics['graph_edit_distance'] = nx.graph_edit_distance(ground_truth, g1)
+
+        return ground_truth_statistics
 
 
 class ResultSchema(BaseSchema):

e1447f55b8fb73eaf1bfbb24d998413798c9a0f5
MariusDanner <marius@danner.online>
2020-01-15 11:03:35 +0100

    Add merge revision

---
+++ services/python-images/migrations/versions/575e6124462f_merge_migrations.py
@@ -0,0 +1,24 @@
+"""merge migrations
+
+Revision ID: 575e6124462f
+Revises: a0d6ed9fe467, b047d3fb4b13
+Create Date: 2020-01-15 11:03:23.741989
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '575e6124462f'
+down_revision = ('a0d6ed9fe467', 'b047d3fb4b13')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

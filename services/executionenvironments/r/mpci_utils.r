e44a9f684097f8e1d052011ae9571fe7d16243e1
constantin-lange <constantin.lange@student.hpi.uni-potsdam.de>
2020-01-30 16:45:58 +0100

    Make HC working

--- services/executionenvironments/r/mpci_utils.r
+++ services/executionenvironments/r/mpci_utils.r
@@ -146,3 +146,30 @@ store_graph_result_bn <- function(api_host, bn_result, df, job_id, independence_
     colorize_log(ANSI_GREEN, paste0('Successfully executed job ', job_id))
     return(graph_request)
 }
+
+store_graph_result_bnlearn_hc <- function(api_host, bn_result, df, job_id, meta_results, execution_time, dataset_loading_time){
+    edge_list <- list(from_node=c(), to_node=c())
+
+    for (i in 1:(length(bn_result$'arcs')/2) ){
+        edge_list[['from_node']][[i]] <- as.numeric(bn_result$'arcs'[i])
+        edge_list[['to_node']][[i]] <- as.numeric(bn_result$'arcs'[i+(length(bn_result$'arcs')/2)])
+        edge_list[['weight']][[i]] <- 0
+    }
+    edge_list <- data.frame(edge_list)
+
+    result_json <- jsonlite::toJSON(list(
+        job_id=strtoi(job_id),
+        edge_list=if(nrow(edge_list) == 0) list() else edge_list,
+        meta_results=meta_results,
+        sepset_list=list(),
+        execution_time =execution_time,
+        dataset_loading_time=dataset_loading_time
+    ), auto_unbox=TRUE)
+
+    graph_request <- RETRY("POST", paste0('http://', api_host, '/api/job/', job_id, '/result'),
+                                 body=result_json, 
+                                 add_headers("Content-Type" = "application/json"), times = 5, quiet=FALSE)
+    check_request(api_host, graph_request, job_id)
+    colorize_log(ANSI_GREEN, paste0('Successfully executed job ', job_id))
+    return(graph_request)
+}

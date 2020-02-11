8d66f0b2d01bf9e4201c5137e5e03f119aa13ae9
Milan Proell <milan.proell@googlemail.com>
2020-02-12 00:16:00 +0100

    job change emits also experiment change

--- services/python-images/src/master/helpers/socketio_events.py
+++ services/python-images/src/master/helpers/socketio_events.py
@@ -1,3 +1,4 @@
-def job_status_change(id, error_code):
+def job_status_change(job, error_code):
     from src.app import socketio
-    socketio.emit('job', {'id': id, 'error_code': error_code})
+    socketio.emit('job', {'id': job.id, 'error_code': error_code})
+    socketio.emit('experiment', {'id': job.experiment_id})

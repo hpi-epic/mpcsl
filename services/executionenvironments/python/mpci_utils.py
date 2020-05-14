e8aaf41a25242f882bf655912a9c9121f17f9e53
Daniel Thevessen <danthe96@gmx.de>
2020-05-14 12:13:04 +0200

    Update debug log

--- services/executionenvironments/python/mpci_utils.py
+++ services/executionenvironments/python/mpci_utils.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import pickle
+import sys
 import time
 from io import StringIO
 
@@ -60,7 +61,7 @@ def store_graph_result(api_host, job_id, graph, exec_time, ds_load_time, args, s
         'execution_time': exec_time,
         'dataset_loading_time': ds_load_time,
     }
-    logging.info(f'Storing graph result:\n {payload}')
+    logging.info(f'Storing graph result... (size {sys.getsizeof(payload)})')
     r = handle_request(lambda s: s.post(f'http://{api_host}/api/job/{job_id}/result', data=json.dumps(payload)),
                        api_host, job_id)
     logging.info(f'Stored graph successfully')

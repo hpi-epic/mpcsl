a8ba9fec03be3d5174681fa43fcd541bb384f74d
Lukas Böhme <lukas.boehme@sap.com>
2020-11-20 11:57:36 +0100

    Added initial dataset generation job

---
+++ services/python-images/src/models/dataset_generation_job.py
@@ -0,0 +1,30 @@
+
+from src.db import db
+from src.models.base import BaseModel, BaseSchema
+from src.models.job import Job, JobSchema
+from src.models.experiment import Experiment
+
+class DatasetGenerationJob(Job):
+    __tablename__ = 'dataset_generation_job'
+    id = db.Column(db.Integer, db.ForeignKey('job.id'), primary_key=True)
+
+    dataset_id = db.Column(
+        db.Integer,
+        db.ForeignKey('dataset.id'),
+        nullable=False
+    )
+    experiment = db.relationship('Dataset', backref=db.backref('job', cascade="all, delete-orphan", uselist=False))
+    
+    nodes = db.Column(db.Integer)
+    samples = db.Column(db.Integer)
+    edgeProbability = db.Column(db.Float)
+    edgeValueLowerBound = db.Column(db.Float)
+    edgeValueUpperBound = db.Column(db.Float)
+
+    __mapper_args__ = {
+        'polymorphic_identity':'dataset_generation_job',
+    }
+
+class DatasetGenerationJobSchema(JobSchema):
+    class Meta(BaseSchema.Meta):
+        model = DatasetGenerationJob

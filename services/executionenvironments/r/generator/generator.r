0e51c31d80c4a5e925a55308fead99e79e6cafa2
Jonas Umland <umland.jonas@gmail.com>
2020-11-20 17:42:48 +0100

    fail if no values provided

--- services/executionenvironments/r/generator/generator.r
+++ services/executionenvironments/r/generator/generator.r
@@ -5,20 +5,22 @@ library(httr)
 tmpDataFile <- 'df.csv'
 
 option_list_v <- list(
-  # optparse does not support mandatory arguments so I set api_host to NA by default to verify later if it was provided.
-  make_option("--apiHost", type = "character", help = "API Host/Port", default = NA),
-  make_option("--nSamples", type = "integer", default = 100, help = "number of samples to be generated"),
-  make_option("--nNodes", type = "integer", default = 5, help = "number of variables"),
-  make_option("--edgeProbability", type = "double", default = 0.5, help = "probability that a given edge is in the graph"),
-  make_option("--edgeValueLowerBound", type = "double", default = -1, help = "lowest possible edge value"),
-  make_option("--edgeValueUpperBound", type = "double", default = 1, help = "highest possible edge value")
+  # optparse does not support mandatory arguments so I set a value to NA by default to verify later if it was provided.
+  make_option("--uploadEndpoint", type = "character", help = "API Host/Port", default = NA),
+  make_option("--nSamples", type = "integer", default = NA, help = "number of samples to be generated"),
+  make_option("--nNodes", type = "integer", default = NA, help = "number of variables"),
+  make_option("--edgeProbability", type = "double", default = NA, help = "probability that a given edge is in the graph"),
+  make_option("--edgeValueLowerBound", type = "double", default = NA, help = "lowest possible edge value"),
+  make_option("--edgeValueUpperBound", type = "double", default = NA, help = "highest possible edge value")
 )
 
 option_parser <- OptionParser(option_list = option_list_v)
 opt <- parse_args(option_parser)
 
-if (is.na(opt$apiHost)) {
-  stop("Argument --apiHost is required")
+for (name in names(opt)) {
+  if(is.na(opt[[name]])){
+    stop(paste0("Paramater --", name, " is required"))
+  }
 }
 
 dag <- randomDAG(opt$nNodes, opt$edgeProbability, opt$edgeValueLowerBound, opt$edgeValueUpperBound)
@@ -26,12 +28,10 @@ dataset <- rmvDAG(opt$nSamples,dag)
 write.csv(dataset, tmpDataFile)
 
 
-post_dataset <- function(apiHost) {
-    url <- paste0('http://', apiHost, '/api/dataset_csv_upload')
+post_dataset <- function(uploadEndpoint) {
+    url <- paste0(uploadEndpoint)
     df_request <- RETRY("POST", url, body = list(file = upload_file(tmpDataFile)), encode = "multipart", times = 1, quiet=FALSE)
     return(df_request)
 }
 
-post_dataset(opt$apiHost)
-
-
+post_dataset(opt$uploadEndpoint)

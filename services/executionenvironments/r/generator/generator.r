bc394f18ce3402cde90675f091288e3668637a2b
Jonas Umland <umland.jonas@gmail.com>
2020-12-04 19:04:21 +0100

    upload ground truth in R image

--- services/executionenvironments/r/generator/generator.r
+++ services/executionenvironments/r/generator/generator.r
@@ -1,8 +1,10 @@
 library("pcalg")
 library(optparse)
 library(httr)
+library(igraph)
 
 tmpDataFile <- 'df.csv'
+tmpGraphFile <- 'graph.gml'
 
 option_list_v <- list(
   # optparse does not support mandatory arguments so I set a value to NA by default to verify later if it was provided.
@@ -27,14 +29,29 @@ dag <- randomDAG(opt$nNodes, opt$edgeProbability, opt$edgeValueLowerBound, opt$e
 dataset <- rmvDAG(opt$nSamples,dag)
 write.csv(dataset, tmpDataFile)
 
+igraphDAG <- igraph.from.graphNEL(dag)
+write_graph(igraphDAG, tmpGraphFile, "gml")
 
-put_dataset <- function(uploadEndpoint) {
-    url <- paste0(uploadEndpoint)
-    df_request <- RETRY("PUT", url, body = list(file = upload_file(tmpDataFile)), encode = "multipart", times = 1, quiet=FALSE)
-    return(df_request)
+upload_dataset <- function(uploadEndpoint) {
+  url <- paste0(uploadEndpoint)
+  response <- RETRY("PUT", url, body = list(file = upload_file(tmpDataFile)), encode = "multipart", times = 1, quiet=FALSE)
+  stop_for_status(response)
+  responseBody <- content(response)
+  if (is.null(responseBody$id)){
+    stop(paste0("Response did not contain dataset id", responseBody))
+  }
+  datasetId <- responseBody$id
+
+  groundTruthEndpoint <- paste0("http://mpci-default-backend/api/dataset/", datasetId, "/upload")
+  response <- RETRY(
+    "POST",
+    groundTruthEndpoint,
+    body = list(graph_file = upload_file(tmpGraphFile)),
+    encode = "multipart",
+    times = 1,
+    quiet=FALSE
+  )
+  stop_for_status(response)
 }
 
-put_dataset(opt$uploadEndpoint)
-#post_ground_truth()
-#igraphDAG <- igraph.from.graphNEL(dag)
-#> write_graph(igraphDAG, "graph.gml", "gml")
+upload_dataset(opt$uploadEndpoint)

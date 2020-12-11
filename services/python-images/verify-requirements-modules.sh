70de8c6501bf2267a72bcb55b98efd9fa7396c93
Jonas Umland <umland.jonas@gmail.com>
2020-12-11 12:17:23 +0100

    dependency verification script

--- services/python-images/verify-requirements-modules.sh
+++ services/python-images/verify-requirements-modules.sh
@@ -1,7 +1,7 @@
 # checks all implicit dependencies are listed in requirements.txt
 # if not, some implicit dependencies may be updated without our knowledge, which led to error in the past
 pip freeze > tmp_requirements.txt
-DIFF=$(diff tmp.txt requirements.txt)
+DIFF=$(diff tmp_requirements.txt requirements.txt)
 if [[ $DIFF ]]; then
     echo "Some dependencies are not in requirements.txt"
     echo "$DIFF"

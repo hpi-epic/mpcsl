4f6bfe7fe70091efa536d6006c4e973effd43047
Jonas Umland <jonas.umland@student.hpi.de>
2021-01-21 17:12:15 +0100

    fix job end timestamp (#270)
    * fix job end timestamp
    
    * fix linting
    
    Co-authored-by: Jonas Umland <umland.jonas@gmail.com>

--- services/python-images/src/master/resources/dataset_generation_job.py
+++ services/python-images/src/master/resources/dataset_generation_job.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from sqlalchemy import func
 
 from src.master.helpers.socketio_events import job_status_change
 from src.models import DatasetGenerationJob, DatasetGenerationJobSchema, Dataset, DatasetSchema
@@ -103,7 +103,7 @@ class DatasetGenerationJobResource(Resource):
         job.dataset = dataset
         job.status = JobStatus.done
 
-        job.end_time = datetime.now()
+        job.end_time = func.now()
 
         add_dataset_nodes(dataset)
 

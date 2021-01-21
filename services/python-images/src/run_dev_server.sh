293ef06125d786680f165d624c83e40276b2e862
Jonas Umland <jonas.umland@student.hpi.de>
2021-01-21 15:10:07 +0100

    Easier dev deployment with minimal K8s setup (#266)
    * dev deploy scripts
    
    * dont deploy backend
    
    Co-authored-by: Jonas Umland <umland.jonas@gmail.com>

--- services/python-images/src/run_dev_server.sh
+++ services/python-images/src/run_dev_server.sh
@@ -1,5 +1,9 @@
+# before running this you should set up a virtualenv and install the requirements.txt
 export POSTGRES_USER=admin
 export POSTGRES_PASSWORD=admin
+export FLASK_APP=migration.py
+cd .. && flask db upgrade && cd src
 export FLASK_APP=app.py
 export FLASK_ENV=development
-flask run
+export SCHEDULER_HOST=localhost:4000
+flask run --host 0.0.0.0

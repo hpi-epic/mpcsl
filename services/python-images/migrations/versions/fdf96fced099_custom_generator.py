af5feda558a9b8f4c9563ec0b4f6af7cb5bcc868
Lukas Böhme <lukas.boehme@icloud.com>
2021-01-22 16:33:56 +0100

    Fixed migrations

--- services/python-images/migrations/versions/fdf96fced099_custom_generator.py
+++ services/python-images/migrations/versions/fdf96fced099_custom_generator.py
@@ -18,8 +18,14 @@ depends_on = None
 
 def upgrade():
 
-    op.add_column('dataset_generation_job', sa.Column('generator_type', sa.String(), nullable=False))
-    op.add_column('dataset_generation_job', sa.Column('parameters', sa.JSON(), nullable=False))
+    op.add_column('dataset_generation_job', sa.Column('generator_type', sa.String(), nullable=True))
+    op.execute('UPDATE dataset_generation_job SET generator_type = \'MPCI\'')
+    op.alter_column('dataset_generation_job', 'generator_type', nullable=False)
+
+    op.add_column('dataset_generation_job', sa.Column('parameters', sa.JSON(), nullable=True))
+    op.execute('UPDATE dataset_generation_job SET parameters = \'{}\'')
+    op.alter_column('dataset_generation_job', 'parameters', nullable=False)
+
     op.drop_column('dataset_generation_job', 'edgeValueLowerBound')
     op.drop_column('dataset_generation_job', 'edgeValueUpperBound')
     op.drop_column('dataset_generation_job', 'nodes')
@@ -30,11 +36,20 @@ def upgrade():
 
 def downgrade():
 
-    op.add_column('dataset_generation_job', sa.Column('edgeProbability', postgresql.DOUBLE_PRECISION(precision=53), autoincrement=False, nullable=False))
-    op.add_column('dataset_generation_job', sa.Column('samples', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('dataset_generation_job', sa.Column('nodes', sa.INTEGER(), autoincrement=False, nullable=False))
-    op.add_column('dataset_generation_job', sa.Column('edgeValueUpperBound', postgresql.DOUBLE_PRECISION(precision=53), autoincrement=False, nullable=False))
-    op.add_column('dataset_generation_job', sa.Column('edgeValueLowerBound', postgresql.DOUBLE_PRECISION(precision=53), autoincrement=False, nullable=False))
+    op.add_column('dataset_generation_job', sa.Column('edgeProbability', postgresql.DOUBLE_PRECISION(precision=53), autoincrement=False, nullable=True))
+    op.add_column('dataset_generation_job', sa.Column('samples', sa.INTEGER(), autoincrement=False, nullable=True))
+    op.add_column('dataset_generation_job', sa.Column('nodes', sa.INTEGER(), autoincrement=False, nullable=True))
+    op.add_column('dataset_generation_job', sa.Column('edgeValueUpperBound', postgresql.DOUBLE_PRECISION(precision=53), autoincrement=False, nullable=True))
+    op.add_column('dataset_generation_job', sa.Column('edgeValueLowerBound', postgresql.DOUBLE_PRECISION(precision=53), autoincrement=False, nullable=True))
+    
+    op.execute('UPDATE dataset_generation_job SET "edgeProbability" = -1, samples = -1, nodes = -1, "edgeValueUpperBound" = -1, "edgeValueLowerBound" = -1')
+
+    op.alter_column('dataset_generation_job', 'edgeProbability', nullable=False)
+    op.alter_column('dataset_generation_job', 'samples', nullable=False)
+    op.alter_column('dataset_generation_job', 'nodes', nullable=False)
+    op.alter_column('dataset_generation_job', 'edgeValueUpperBound', nullable=False)
+    op.alter_column('dataset_generation_job', 'edgeValueLowerBound', nullable=False)
+
     op.drop_column('dataset_generation_job', 'parameters')
     op.drop_column('dataset_generation_job', 'generator_type')
 

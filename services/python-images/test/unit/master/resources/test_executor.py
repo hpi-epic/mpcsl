c80a35f75a1679a05e4208610963dc9ec14ab3c7
Mats Pörschke <ma-poerschke@t-online.de>
2021-02-25 12:51:01 +0100

    Define column name

--- services/python-images/test/unit/master/resources/test_executor.py
+++ services/python-images/test/unit/master/resources/test_executor.py
@@ -9,10 +9,11 @@ class ExecutorTest(BaseResourceTest):
     def test_returns_all_algorithms(self):
         # Given
         ex = ExperimentFactory()
-        ex.dataset.load_query = 'SELECT 5'
+        ex.dataset.load_query = 'SELECT 5 AS col1_name'
         ex.dataset.content_hash = "cee5da8b8b5d8089f7cd7c9c85fac0e390ea2c4d6c7b" + \
             "04edad1a09796705a832017344e04d4aab4d0adcac636466dc70a9d952ae417254a1d877fbe99eb3b750"
-        self.post(self.url_for(ExecutorResource, experiment_id=ex.id))
+        res = self.post(self.url_for(ExecutorResource, experiment_id=ex.id))
+        res.raise_for_status()
 
         # Then
         assert ex.last_job.status == JobStatus.waiting

23d8e47d8cf45bf55f6f4203c9d44b3de5e43639
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2023-01-12 17:16:14 +0100

    adjust handling of setup algorithm

--- services/python-images/setup_algorithms.py
+++ services/python-images/setup_algorithms.py
@@ -12,22 +12,24 @@ def set_up_algorithms(db):
         # The check here is necessary, because without running the migrations,
         # the DB might be empty. To be able to run the migrations though, it is necessary to be able
         # to initialize the app.
-        if db.engine.dialect.has_table(db.session, Algorithm.__table__.name):
+        if db.engine.dialect.has_table(db.session.connection(), Algorithm.__table__.name):
             with open('conf/algorithms.json') as f:
                 algorithms = json.load(f)
                 for algorithm in algorithms:
-                    data, errors = AlgorithmSchema().load(algorithm)
-                    if len(errors) > 0:
-                        raise InvalidInputData(payload=errors)
-                    alg_in_db = db.session.query(Algorithm).filter(Algorithm.package == data['package'])\
-                        .filter(Algorithm.function == data['function']).one_or_none()
-                    if not alg_in_db:
-                        alg = Algorithm(**data)
-                        db.session.add(alg)
-                        print(f'==> Function `{data["function"]}` from Package `{data["package"]}` added…')
-                    else:
-                        alg_in_db.update(data)
-                        print(f'==> Function `{data["function"]}` from Package `{data["package"]}` updated…')
+                    print(AlgorithmSchema().load(algorithm))
+                    try:
+                        data = AlgorithmSchema().load(algorithm)
+                        alg_in_db = db.session.query(Algorithm).filter(Algorithm.package == data['package'])\
+                            .filter(Algorithm.function == data['function']).one_or_none()
+                        if not alg_in_db:
+                            alg = Algorithm(**data)
+                            db.session.add(alg)
+                            print(f'==> Function `{data["function"]}` from Package `{data["package"]}` added…')
+                        else:
+                            alg_in_db.update(data)
+                            print(f'==> Function `{data["function"]}` from Package `{data["package"]}` updated…')
+                    except Exception as err:
+                        raise InvalidInputData(payload=err)
                 db.session.commit()
 
 
